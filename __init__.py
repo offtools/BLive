ae4d0365e059ff68a4f6bf84f7ba14c5bb4255f0
Thomas Achtner <info@offtools.de>
2012-05-03 23:12:45 +0200

    updated blender version tag in bl_info, updated new modules import

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ bl_info = {
 	"name": "BLive",
 	"author": "offtools",
 	"version": (0, 0, 1),
-	"blender": (2, 6, 0),
+	"blender": (2, 6, 3),
 	"location": "various Panels with prefix BLive",
 	"description": "blender to bge osc network addon",
 	"warning": "",
@@ -50,6 +50,8 @@ if "bpy" in locals():
 	imp.reload('texture')
 	imp.reload('meshtools')
 	imp.reload('apphandler')
+	imp.reload('properties')
+	imp.reload('network')
 else:
 	from . import logic
 	from . import client
@@ -57,114 +59,117 @@ else:
 	from . import texture
 	from . import meshtools
 	from . import apphandler
-   
+	from . import properties
+	from . import network
+
 import bpy
-import sys
-import subprocess
+#import sys
+#import subprocess
 
-#
-#	Scene Network Panel
-#
-class BLive_PT_scene_network(bpy.types.Panel):
-	bl_label = "BLive Network"
-	bl_space_type = 'PROPERTIES'
-	bl_region_type = 'WINDOW'
-	bl_context = "scene"
+##
+##	Scene Network Panel
+##
+#class BLive_PT_scene_network(bpy.types.Panel):
+#	bl_label = "BLive Network"
+#	bl_space_type = 'PROPERTIES'
+#	bl_region_type = 'WINDOW'
+#	bl_context = "scene"
 
-	def draw(self, context):
-		self.layout.label(text="Setup")
+#	def draw(self, context):
+#		self.layout.label(text="Setup")
 
-		box = self.layout.box()
+#		box = self.layout.box()
 
-		if "PORT" in bpy.context.scene.camera.game.properties:
-			row = box.row()
-			row.prop(bpy.context.scene.camera.game.properties["PORT"], "value", text="Port: ")
-			row.operator("blive.set_port", text="change port")
-			row.operator("blive.logic_remove", text="", icon="X")
-		else:
-			box.label("add OSC logic to: {}".format(bpy.context.scene.camera.name))
-			row = box.row()
-			row.operator("blive.logic_add", text="create scripts")
-		
-		if "PORT" not in bpy.context.scene.camera.game.properties:
-			return
-			
-		row = self.layout.column()
-		row.label("Next Steps:")
+#		if "PORT" in bpy.context.scene.camera.game.properties:
+#			row = box.row()
+#			row.prop(bpy.context.scene.camera.game.properties["PORT"], "value", text="Port: ")
+#			row.operator("blive.set_port", text="change port")
+#			row.operator("blive.logic_remove", text="", icon="X")
+#		else:
+#			box.label("add OSC logic to: {}".format(bpy.context.scene.camera.name))
+#			row = box.row()
+#			row.operator("blive.logic_add", text="create scripts")
+#		
+#		if "PORT" not in bpy.context.scene.camera.game.properties:
+#			return
+#			
+#		row = self.layout.column()
+#		row.label("Next Steps:")
 
-		row = self.layout.row()
-		split = row.split(percentage=0.1)
-		split.label("1.")
-		split = split.split()
-		split.operator_context = 'INVOKE_AREA'
-		split.operator("wm.save_as_mainfile", text="Save As...")		
-		
-		row = self.layout.row()
-		split = row.split(percentage=0.1)
-		split.label("2.")
-		split = split.split()
-		if "PORT" not in bpy.context.scene.camera.game.properties or not bpy.context.blend_data.filepath:
-			split.enabled = False		
-		split.operator("blive.fork_blenderplayer", text="Start")
-		
-		row = self.layout.row()
-		split = row.split(percentage=0.1)
-		split.label("3.")
-		split = split.split()
-		split.operator("blive.quit", text="Quit")
+#		row = self.layout.row()
+#		split = row.split(percentage=0.1)
+#		split.label("1.")
+#		split = split.split()
+#		split.operator_context = 'INVOKE_AREA'
+#		split.operator("wm.save_as_mainfile", text="Save As...")		
+#		
+#		row = self.layout.row()
+#		split = row.split(percentage=0.1)
+#		split.label("2.")
+#		split = split.split()
+#		if "PORT" not in bpy.context.scene.camera.game.properties or not bpy.context.blend_data.filepath:
+#			split.enabled = False		
+#		split.operator("blive.fork_blenderplayer", text="Start")
+#		
+#		row = self.layout.row()
+#		split = row.split(percentage=0.1)
+#		split.label("3.")
+#		split = split.split()
+#		split.operator("blive.quit", text="Quit")
 
-class BLive_OT_forc_blenderplayer(bpy.types.Operator):
-	bl_idname = "blive.fork_blenderplayer"
-	bl_label = "BLive fork blenderplayer"
+#class BLive_OT_forc_blenderplayer(bpy.types.Operator):
+#	bl_idname = "blive.fork_blenderplayer"
+#	bl_label = "BLive fork blenderplayer"
 
-	def execute(self, context):
-		if "PORT" in bpy.context.scene.camera.game.properties:
-			client.client().port = bpy.context.scene.camera.game.properties["PORT"].value
-			app = "blenderplayer"
-			blendfile = bpy.context.blend_data.filepath
-			port = "-p {0}".format(bpy.context.scene.camera.game.properties["PORT"].value)
-			cmd = [app,  port, blendfile]
-			blendprocess = subprocess.Popen(cmd)
+#	def execute(self, context):
+#		if "PORT" in bpy.context.scene.camera.game.properties:
+#			client.client().port = bpy.context.scene.camera.game.properties["PORT"].value
+#			app = "blenderplayer"
+#			blendfile = bpy.context.blend_data.filepath
+#			port = "-p {0}".format(bpy.context.scene.camera.game.properties["PORT"].value)
+#			cmd = [app,  port, blendfile]
+#			blendprocess = subprocess.Popen(cmd)
 
-			return{'FINISHED'}
-		else:
-			return{'CANCELLED'}
+#			return{'FINISHED'}
+#		else:
+#			return{'CANCELLED'}
 
-class BLive_OT_set_port(bpy.types.Operator):
-	bl_idname = "blive.set_port"
-	bl_label = "BLive set OSC port"
+#class BLive_OT_set_port(bpy.types.Operator):
+#	bl_idname = "blive.set_port"
+#	bl_label = "BLive set OSC port"
 
-	def execute(self, context):
-		if "PORT" in bpy.context.scene.camera.game.properties:
-			client.client().port = bpy.context.scene.camera.game.properties["PORT"].value
-			return{'FINISHED'}
-		else:
-			return{'CANCELLED'}
+#	def execute(self, context):
+#		if "PORT" in bpy.context.scene.camera.game.properties:
+#			client.client().port = bpy.context.scene.camera.game.properties["PORT"].value
+#			return{'FINISHED'}
+#		else:
+#			return{'CANCELLED'}
 
-class BLive_OT_quit(bpy.types.Operator):
-	bl_idname = "blive.quit"
-	bl_label = "BLive quit blenderplayer"
+#class BLive_OT_quit(bpy.types.Operator):
+#	bl_idname = "blive.quit"
+#	bl_label = "BLive quit blenderplayer"
 
-	def execute(self, context):
-		if "PORT" in bpy.context.scene.camera.game.properties:
-			client.client().quit()
-			# TODO unregister app handlers
-			for i in bpy.app.handlers.frame_change_post:
-				bpy.app.handlers.frame_change_post.remove(i)
-			for i in bpy.app.handlers.scene_update_post:
-				bpy.app.handlers.scene_update_post.remove(i)
-			return{'FINISHED'}
-		else:
-			return{'CANCELLED'}
+#	def execute(self, context):
+#		if "PORT" in bpy.context.scene.camera.game.properties:
+#			client.client().quit()
+#			# TODO unregister app handlers
+#			for i in bpy.app.handlers.frame_change_post:
+#				bpy.app.handlers.frame_change_post.remove(i)
+#			for i in bpy.app.handlers.scene_update_post:
+#				bpy.app.handlers.scene_update_post.remove(i)
+#			return{'FINISHED'}
+#		else:
+#			return{'CANCELLED'}
 
 def register():
+	properties.register()
 	bpy.utils.register_module(__name__)
 	apphandler.register()
 
 def unregister():
 	apphandler.unregister()
+	properties.unregister()
 	bpy.utils.unregister_module(__name__)
 
 if __name__ == "__main__":
-	print("registering blive modules")
-	register()
+	pass
