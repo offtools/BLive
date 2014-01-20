142dab7c75322bfb9c363a364022accbfba6257f
Thomas Achtner <info@offtools.de>
2014-01-20 16:43:07 +0100

    tabs

--- marker/__init__.py
+++ marker/__init__.py
@@ -22,36 +22,35 @@
 #
 #   BLive: Timeline Marker / Trigger Panel located in NLA Editor UI region
 #
-#	The Panel is used to add extra functions to timeline and timeline markers,
-#	Like pause playback when reaching a timeline marker and send OSC Messages to
-#	the BGE instance (start/stop movies, change Scene, ...)
+#   The Panel is used to add extra functions to timeline and timeline markers,
+#   Like pause playback when reaching a timeline marker and send OSC Messages to
+#   the BGE instance (start/stop movies, change Scene, ...)
 #
 
 # import modules
 if "bpy" in locals():
-	print("imp.reload")
-	import imp
-	imp.reload(props)
-	imp.reload(ops)
-	imp.reload(ui)
-	imp.reload(handler)
+    print("imp.reload")
+    import imp
+    imp.reload(props)
+    imp.reload(ops)
+    imp.reload(ui)
+    imp.reload(handler)
 else:
-	print("import")
-	from . import props
-	from . import ops
-	from . import ui
-	from . import handler
+    from . import props
+    from . import ops
+    from . import ui
+    from . import handler
 
 def register():
-	print("marker.register")
-	props.register()
-	ops.register()
-	ui.register()
-	handler.register()
-	
+    print("marker.register")
+    props.register()
+    ops.register()
+    ui.register()
+    handler.register()
+
 def unregister():
-	print("marker.unregister")
-	ui.unregister()
-	ops.unregister()
-	props.unregister()
-	handler.unregister()
+    print("marker.unregister")
+    ui.unregister()
+    ops.unregister()
+    props.unregister()
+    handler.unregister()
